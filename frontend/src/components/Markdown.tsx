07954992be53aa2fc7cdd3237f4405d615e74a04
Cyrusbye720 <byecyrus@gmail.com>
2026-06-02 22:04:27 +0530

    fix: react warnings by stripping node prop from markdown components

--- frontend/src/components/Markdown.tsx
+++ frontend/src/components/Markdown.tsx
@@ -18,16 +18,16 @@ export default function Markdown({ content }: MarkdownProps) {
         remarkPlugins={[remarkGfm, remarkGithubAlerts]}
         rehypePlugins={[rehypeRaw, rehypeSlug]}
         components={{
-          h1: ({ ...props }) => <h1 className={s.h1} {...props} />,
-          h2: ({ ...props }) => <h2 className={s.h2} {...props} />,
-          h3: ({ ...props }) => <h3 className={s.h3} {...props} />,
-          p: ({ ...props }) => <p className={s.paragraph} {...props} />,
-          ul: ({ ...props }) => <ul className={s.listUl} {...props} />,
-          ol: ({ ...props }) => <ol className={s.listOl} {...props} />,
-          li: ({ ...props }) => <li className={s.listItem} {...props} />,
-          blockquote: ({ ...props }) => <blockquote className={s.blockquote} {...props} />,
-          pre: ({ ...props }) => <pre className={s.codeBlock} {...props} />,
-          code: ({ className, ...props }) => {
+          h1: ({ node, ...props }) => <h1 className={s.h1} {...props} />,
+          h2: ({ node, ...props }) => <h2 className={s.h2} {...props} />,
+          h3: ({ node, ...props }) => <h3 className={s.h3} {...props} />,
+          p: ({ node, ...props }) => <p className={s.paragraph} {...props} />,
+          ul: ({ node, ...props }) => <ul className={s.listUl} {...props} />,
+          ol: ({ node, ...props }) => <ol className={s.listOl} {...props} />,
+          li: ({ node, ...props }) => <li className={s.listItem} {...props} />,
+          blockquote: ({ node, ...props }) => <blockquote className={s.blockquote} {...props} />,
+          pre: ({ node, ...props }) => <pre className={s.codeBlock} {...props} />,
+          code: ({ node, className, ...props }) => {
             const isInline = !className?.includes('language-');
             return isInline ? (
               <code className={s.inlineCode} {...props} />
@@ -35,12 +35,15 @@ export default function Markdown({ content }: MarkdownProps) {
               <code className={className} {...props} />
             );
           },
-          a: ({ ...props }) => <a className={s.link} target="_blank" rel="noopener noreferrer" {...props} />,
-          img: ({ ...props }) => <img className={s.contentImage} loading="lazy" {...props} />,
-          hr: ({ ...props }) => <hr className={s.hr} {...props} />,
-          table: ({ ...props }) => <div className={s.tableWrapper}><table className={s.table} {...props} /></div>,
-          th: ({ ...props }) => <th className={s.th} {...props} />,
-          td: ({ ...props }) => <td className={s.td} {...props} />,
+          a: ({ node, ...props }) => <a className={s.link} target="_blank" rel="noopener noreferrer" {...props} />,
+          img: ({ node, ...props }) => <img className={s.contentImage} loading="lazy" {...props} />,
+          hr: ({ node, ...props }) => <hr className={s.hr} {...props} />,
+          table: ({ node, ...props }) => <div className={s.tableWrapper}><table className={s.table} {...props} /></div>,
+          thead: ({ node, ...props }) => <thead className={s.thead} {...props} />,
+          tbody: ({ node, ...props }) => <tbody className={s.tbody} {...props} />,
+          tr: ({ node, ...props }) => <tr className={s.tr} {...props} />,
+          th: ({ node, ...props }) => <th className={s.th} {...props} />,
+          td: ({ node, ...props }) => <td className={s.td} {...props} />,
         }}
       >
         {content}
